cab69aff6464faffef8ac44ff527c637a441e2ea
fforelle <xdollboyx@gmail.com>
2023-03-14 20:04:09 -0300

    Configurando cores tailwind e prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      white: "#FFFFFF",
+      black: "#1A1A1A",
+      grey: "#5B5B5B",
+      grey_light: "#666666",
+      green_light: "#B1E7C4",
+      card_red: "#B13E53",
+      card_green: "#38B764",
+      card_blue: "#3B5DC9",
+      card_orange: "#EF7D57",
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+        raleway: ["Raleway"],
+      },
+    },
   },
   plugins: [],
-}
+};
