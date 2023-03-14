4737e8e5541e4edd976f22deb099a40b38b5a565
fforelle <xdollboyx@gmail.com>
2023-03-14 17:14:29 -0300

    Adicionando paleta de cores e fonts no tailwind.config

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Cadastro from './pages/Cadastro';
-import Home from './pages/Home';
+import Cadastro from "./pages/Cadastro";
+import Home from "./pages/Home";
 
 export default function Router() {
-	return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home/>} />
-                <Route path='/Cadastro' element={<Cadastro/>} />
-            </Routes>
-        </BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
