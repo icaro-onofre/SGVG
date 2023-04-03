7fd215c9b4f95bdd219bdc27e65075020e3dbef7
dannesx <daniel.viantunes@gmail.com>
2023-04-03 00:05:35 -0300

    Adicionando dark mode atualizado

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class',
   theme: {
     colors: {
       white: '#f6f6f6',
-      black: '#131313',
+      black: '#161616',
       jade: '#52a675',
       red: '#ff595e',
       yellow: '#ffca3a',
       green: '#8ac926',
       blue: '#1982c4',
       violet: '#6a4c93',
+      // Variantes no tema escuro
+      dark_white: '#ffffff',
+      dark_black: '#161616',
+      dark_jade: '#ff7377',
+      dark_red: '#ffd154',
+      dark_yellow: '#98D831',
+      dark_green: '#61b182',
+      dark_blue: '#1c92db',
+      dark_violet: '#7655a4',
     },
     extend: {
       fontFamily: {
