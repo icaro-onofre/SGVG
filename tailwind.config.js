1e59ef95f8584091959b368c169099942dd452b6
dannesx <daniel.viantunes@gmail.com>
2023-04-02 23:50:00 -0300

    Atualizando padrões de cores e fontes - Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
     colors: {
-      white: "#FFFFFF",
-      black: "#1A1A1A",
-      grey: "#5B5B5B",
-      grey_light: "#666666",
-      green_light: "#B1E7C4",
-      card_red: "#B13E53",
-      card_green: "#38B764",
-      card_blue: "#3B5DC9",
-      card_orange: "#EF7D57",
+      white: '#f6f6f6',
+      black: '#131313',
+      jade: '#52a675',
+      red: '#ff595e',
+      yellow: '#ffca3a',
+      green: '#8ac926',
+      blue: '#1982c4',
+      violet: '#6a4c93',
     },
     extend: {
       fontFamily: {
-        poppins: ["Poppins"],
-        raleway: ["Raleway"],
+        body: ['"Open Sans"'],
+        heading: ['Raleway'],
       },
     },
   },
