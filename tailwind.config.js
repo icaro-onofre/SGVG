ab375d4d0cdec74a482e1fdacab3293deae50ba6
Icaro <icaro.onofre.s@gmail.com>
2023-04-02 23:03:18 -0300

    Terminando darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,16 @@ module.exports = {
       card_green: '#38B764',
       card_blue: '#3B5DC9',
       card_orange: '#EF7D57',
+      // Variantes no tema escuro
+      dark_white: '#FFFFFF',
+      dark_black: '#1A1A1A',
+      dark_grey: '#5B5B5B',
+      dark_grey_light: '#666666',
+      dark_green_light: '#B1E7C4',
+      dark_card_red: '#B13E53',
+      dark_card_green: '#38B764',
+      dark_card_blue: '#3B5DC9',
+      dark_card_orange: '#EF7D57',
     },
     extend: {
       fontFamily: {
