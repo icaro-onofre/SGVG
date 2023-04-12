31913af07b32d3e2438fc92b9a8f6d68e292f220
Icaro <icaro.onofre.s@gmail.com>
2023-04-11 21:03:16 -0300

    Adicionando conexão com api e router guard

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,34 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { React, useState, useEffect } from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import axiosInstance from 'services/axios';
 
-import Cadastro from "./pages/Cadastro";
-import Home from "./pages/Home";
+import Cadastro from './pages/Cadastro';
+import Home from './pages/Home';
+
+let token = JSON.parse(localStorage.getItem('token'));
 
 export default function Router() {
+
+  function hasJWT() {
+    let flag = false;
+
+    //Checa a existência do JWT token
+    localStorage.getItem('token') ? (flag = true) : (flag = false);
+
+    return flag;
+  }
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/cadastro" element={<Cadastro />} />
+        <Route element={
+		hasJWT() 
+		? <Route path="/cadastro" element={<Cadastro/>}/> 
+		: <Navigate to="/" />		
+		}
+	  />
+        
       </Routes>
     </BrowserRouter>
   );
