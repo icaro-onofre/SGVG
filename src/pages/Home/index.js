472185ec8712ea1236e1507791c875002ca84aa2
Icaro <icaro.onofre.s@gmail.com>
2023-04-13 15:18:21 -0300

    Adicionando renderização condicional das vagas retornadas da API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,74 +1,33 @@
-import signIn from 'services/funcionario';
-import { useNavigate } from "react-router-dom";
-
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
+import VagaCard from 'components/VagaCard';
+import Legenda from 'components/VagaCard/Legenda';
+import { getVagas } from 'services/vaga.js';
+import axiosInstance from 'services/axios';
 
 export default function Home() {
-  const [nome, setNome] = useState('');
-  const [senha, setSenha] = useState('');
+  const [vagas, setVagas] = useState([]);
 
-  let navigate = useNavigate();
-
-  function handleSetNome(e) {
-    e.preventDefault();
-    setNome(e.target.value);
-  }
-
-  function handleSetSenha(e) {
-    e.preventDefault();
-    setSenha(e.target.value);
-  }
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    try {
-      signIn(nome, senha);
-      navigate('/cadastro');
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  const getVagas = () => {
+    axiosInstance
+      .get('/vaga')
+      .then((res) => setVagas(res.data))
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    getVagas();
+  }, []);
 
   return (
-    <div>
-      {' '}
-      <main className="mx-auto flex min-h-screen w-full items-center justify-center bg-white text-green">
-        {/*fundo branco e cor do texto verde escuro*/}
-        <form onSubmit={handleSubmit} className="flex w-[30rem] flex-col space-y-10">
-          {' '}
-          {/*seção de login*/}
-          <img className="mx-auto h-40 w-auto" src="login.png" /> {/*logo do aplicativo escrito login e alinhado*/}
-          <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-green">
-            {/*caixa de texto, campo de login/ mais efeito mudando de cor quando clicar no campo*/}
-            <input
-              type="text"
-              placeholder="Nome de Usuário"
-              className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
-              onChange={handleSetNome}
-            />
-          </div>
-          <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-green">
-            {/*caixa de texto, campo de senha/ mais efeito mudando de cor quando clica no campo*/}
-            <input
-              type="password"
-              placeholder="Senha"
-              className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
-              onChange={handleSetSenha}
-            />
-          </div>
-          <button
-            className="transform rounded-sm bg-green py-2 font-bold duration-300 hover:bg-green text-white text-center"
-            type="submit"
-          >
-            Conecte-se
-          </button>
-          {/*botão de login*/}
-          <a className="transform text-center font-semibold text-gray-500 duration-300 hover:text-green">
-            ESQUECEU A SENHA?
-          </a>
-          {/*link para cadastro não definido ainda*/}
-        </form>
-      </main>
-    </div>
+    <>
+      <p className="mx-32 my-5 font-bold">STATUS DAS VAGAS</p>
+      <div className="grid grid-cols-12 ">
+        <div className="flex flex-row flex-wrap col-start-2  col-end-12 h-screen">
+          {vagas.map((key, vaga) => {
+            return <VagaCard key={key} nome={vaga.preco} />;
+          })}
+        </div>
+      </div>
+      <Legenda />
+    </>
   );
 }
