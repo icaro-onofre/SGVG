a9c9e945dc969367d4f57e470513338fab1a6b1f
Icaro <icaro.onofre.s@gmail.com>
2023-04-13 18:35:37 -0300

    Removendo variavel 'ocupada' de componente

--- src/components/VagaCard/index.js
+++ src/components/VagaCard/index.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 export default function VagaCard(props) {
-  
-let ocupada = props.vaga_ocupada
   return (
-    <div className={ocupada ? 'bg-red h-16 w-16 rounded-lg m-4'  : 'bg-green  h-16  w-16  rounded-lg m-4'}>
-      <div className={"flex items-center justify-center m-5 text-center text-white"}>{props.nome}</div>
+    <div className={props.vaga_ocupada ? 'bg-red h-16 w-16 rounded-lg m-4' : 'bg-green  h-16  w-16  rounded-lg m-4'}>
+      <div className={'flex items-center justify-center m-5 text-center text-white'}>{props.nome}</div>
     </div>
-  )
+  );
 }
