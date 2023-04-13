b518fbc74c848d20d8f09a018174dd3281434bca
Icaro <icaro.onofre.s@gmail.com>
2023-04-13 18:14:14 -0300

    Adicionando renderização condicional com base no status da vaga

--- src/components/VagaCard/index.js
+++ src/components/VagaCard/index.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 
 export default function VagaCard(props) {
+  
+let ocupada = props.vaga_ocupada
   return (
-    <div className="h-16  w-16 bg-red rounded-lg m-4">
-      <div className="flex items-center justify-center text-white m-5 text-center">{props.nome}</div>
+    <div className={ocupada ? 'bg-red h-16 w-16 rounded-lg m-4'  : 'bg-green  h-16  w-16  rounded-lg m-4'}>
+      <div className={"flex items-center justify-center m-5 text-center text-white"}>{props.nome}</div>
     </div>
   )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,6 +5,7 @@ import { getVagas } from 'services/vaga.js';
 import axiosInstance from 'services/axios';
 
 export default function Home() {
+
   const [vagas, setVagas] = useState([]);
 
   const getVagas = () => {
@@ -13,6 +14,7 @@ export default function Home() {
       .then((res) => setVagas(res.data))
       .catch((err) => console.log(err));
   };
+	
   useEffect(() => {
     getVagas();
   }, []);
@@ -23,8 +25,16 @@ export default function Home() {
       <div className="grid grid-cols-12 ">
         <div className="flex flex-row flex-wrap col-start-2  col-end-12 h-screen">
           {vagas.map((key, vaga) => {
-            return <VagaCard key={key} nome={vaga.preco} />;
-          })}
+            return (
+		    <VagaCard 
+		    key={key} 
+		    nome={vagas[vaga].setor} 
+		    vaga_ocupada={vagas[vaga].vaga_ocupada}
+		    />
+	    );
+          })
+
+	  }
         </div>
       </div>
       <Legenda />
