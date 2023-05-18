60215d72cf2d97431248a40180fc209fb029b0e1
Icaro <icaro.onofre.s@gmail.com>
2023-05-17 23:26:22 -0300

    [wip] Renderizando dados retornados nos campos

--- src/components/Modal/ModalFuncionario/index.js
+++ src/components/Modal/ModalFuncionario/index.js
@@ -12,63 +12,86 @@ export default function ModalFuncionario(props) {
   const [selectedFuncionarioId, setSelectedFuncionarioId] = useAtom(funcionarioId);
   const [selectedFuncionarioDataFiltered, setSelectedFuncionarioDataFiltered] = useAtom(funcionarioDataFiltered);
 
+  const [funcionarios, setFuncionarios] = useState([]);
+
   const handleSetFoldFuncionario = () => {
     setFoldFuncionario(!foldFuncionario);
   };
 
-  const [funcionarios, setFuncionarios] = useState([]);
-
-  const getFuncionarioFiltered = () => {};
-
   useEffect(() => {
-    if (foldFuncionario == true) {
-      axiosInstance
-        .post('/funcionario/filter', {
-          id: selectedFuncionarioId,
-          cargo: null,
-          cpf: null,
-          data_nasc: null,
-          idade: null,
-          nome: null,
-          root: null,
-        })
-        .then((res) => setSelectedFuncionarioDataFiltered(res.data))
-        .catch((err) => console.log(err));
-    }
+    axiosInstance
+      .post('/funcionario/filter', {
+        id: selectedFuncionarioId,
+        cargo: null,
+        cpf: null,
+        data_nasc: null,
+        idade: null,
+        nome: null,
+        root: null,
+      })
+      .then((res) => setSelectedFuncionarioDataFiltered(res.data))
+      .catch((err) => console.log(err));
   }, [foldFuncionario]);
 
-  console.log(selectedFuncionarioDataFiltered);
-
-  console.log(selectedFuncionarioId);
   return (
     <div className={' inset-0' + foldFuncionario ? 'bg-opacity-40' : ''}>
       {!foldFuncionario ? (
         <div></div>
       ) : (
         <div className="absolute w-screen h-screen bg-black/[0.85] z-20 inset-0 flex items-center justify-center transition duration-100 ease-in">
-          <button className="absolute w-screen h-screen z-0 bg-black/[0.85]" onClick={handleSetFoldFuncionario} />
-          <div className=" w-1/2 h-1/2 bg-white rounded-xl z-20">
-            <h1 className="text-2xl font-bold ml-5 mt-4">Editar funcionario</h1>
-            <div className="ml-8">
-              <div className=" space-y-1 h-90 mt-8 ">
-                <div className="flex flex-row space-x-5">
-                  <Input placeholder="Nome" />
-                  <Input placeholder="CPF" />
-                </div>
-                <div className="flex flex-row space-x-5">
-                  <Input placeholder="Senha" />
-                  <Input placeholder="Idade" />
-                </div>
-                <div className="flex flex-row space-x-5">
-                  <Input placeholder="DataNasc" />
-                  <Input placeholder="Cargo" />
+          {selectedFuncionarioDataFiltered.length === 0 ? (
+            <>
+              <button className="absolute w-screen h-screen z-0 bg-black/[0.85]" onClick={handleSetFoldFuncionario} />
+              <div className=" w-1/2 h-1/2 bg-white rounded-xl z-20">
+                <h1 className="text-2xl font-bold ml-5 mt-4">Editar funcionario</h1>
+                <div className="ml-8">
+                  <div className=" space-y-1 h-90 mt-8 ">
+                    <div className="flex flex-row space-x-5">
+                      <Input placeholder="Nome" />
+                      <Input placeholder="CPF" />
+                    </div>
+                    <div className="flex flex-row space-x-5">
+                      <Input placeholder="Senha" />
+                      <Input placeholder="Idade" />
+                    </div>
+                    <div className="flex flex-row space-x-5">
+                      <Input placeholder="DataNasc" />
+                      <Input placeholder="Cargo" />
+                    </div>
+                  </div>
+                  <div className="mt-7 ml-72">
+                    <Button value="Editar funcionario" />
+                  </div>
                 </div>
               </div>
-              <div className="mt-7 ml-72">
-                <Button value="Editar funcionario" />
+            </>
+          ) : (
+            <>
+              <button className="absolute w-screen h-screen z-0 bg-black/[0.85]" onClick={handleSetFoldFuncionario} />
+              <div className=" w-1/2 h-1/2 bg-white rounded-xl z-20">
+                <h1 className="text-2xl font-bold ml-5 mt-4">Editar funcionario</h1>
+                <div className="ml-8">
+                  <div className=" space-y-1 h-90 mt-8 ">
+                    <div className="flex flex-row space-x-5">
+                      <Input placeholder={selectedFuncionarioDataFiltered[0].id} />
+                      <Input placeholder={selectedFuncionarioDataFiltered[0].cpf} />
+                    </div>
+                    <div className="flex flex-row space-x-5">
+                      <Input placeholder={selectedFuncionarioDataFiltered[0].senha} />
+                      <Input placeholder={selectedFuncionarioDataFiltered[0].idade} />
+                    </div>
+                    <div className="flex flex-row space-x-5">
+                      <Input placeholder={selectedFuncionarioDataFiltered[0].data_nasc} />
+                      <Input placeholder="Cargo" />
+                    </div>
+                  </div>
+                  <div className="mt-7 ml-72">
+                    <Button value="Editar funcionario" />
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            </>
+          )}
         </div>
       )}
     </div>
--- src/pages/Funcionario/index.js
+++ src/pages/Funcionario/index.js
@@ -5,12 +5,21 @@ import Table from 'components/Table/Funcionario/';
 import Button from 'components/Button';
 import ModalFuncionario from 'components/Modal/ModalFuncionario';
 import axiosInstance from 'services/axios';
+import { useAtom } from 'jotai';
+import { colapsedFuncionario } from 'store.js';
 
 export default function Funcionario() {
+  const [foldFuncionario, setFoldFuncionario] = useAtom(colapsedFuncionario);
   return (
     <div className="overflow-hidden h-screen  grid grid-cols-12">
       <div className="col-start-1 col-end-13">
-        <ModalFuncionario />
+        {!foldFuncionario ? (
+          <div></div>
+        ) : (
+          <div>
+            <ModalFuncionario />
+          </div>
+        )}
         <Header />
         <Navbar />
       </div>
